9b9d0e96206c0d59cbdef2ce417731549f1fdf8f
unknown <tjdvy2829@naver.com>
2024-09-02 16:54:42 +0900

    Base Parkour systems

---
+++ parkour/Assets/Script/Parkour System/ParkourAction.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ParkourAction_" ,menuName = "Scriptable Object/Parkour System/New parkour action")]
+public class ParkourAction : ScriptableObject
+{
+    [SerializeField] string animName;
+    [SerializeField] string obstacleTag;
+
+    [SerializeField] float minHeight;
+    [SerializeField] float maxHeight;
+
+    [SerializeField] bool rotateToObstacle;
+    [SerializeField] float postActionDelay;
+
+    #region Header TARGET MATCHING
+    [Space(10)]
+    [Header("Target Matching")]
+    [SerializeField] bool enableTargetMatching = true;
+    [SerializeField] AvatarTarget matchBodyPart;
+    [SerializeField] float matchStartTime;
+    [SerializeField] float matchTargetTime;
+    [SerializeField] Vector3 matchPosWeight = new Vector3(0, 1, 0);
+    #endregion
+
+    public Quaternion TargetRotation { get; set; }
+    public Vector3 MatchPos { get; set; }
+
+    public bool CheckIfPossible(ObstacleHitData hitData, Transform player)
+    {
+        // Check Tag
+        if (!string.IsNullOrEmpty(obstacleTag) && hitData.forwardHit.transform.tag != obstacleTag)
+            return false;
+
+        // Height Tag
+        float height = hitData.heightHit.point.y - player.position.y;
+        if (height < minHeight || height > maxHeight)
+            return false;
+
+        if (rotateToObstacle)
+            TargetRotation = Quaternion.LookRotation(-hitData.forwardHit.normal);
+
+        if (enableTargetMatching)
+            MatchPos = hitData.heightHit.point;
+
+        return true;
+    }
+
+    public string AnimName => animName;
+    public bool RotateToObstacle => rotateToObstacle;
+    public float PostActionDelay => postActionDelay;
+
+    public bool EnableTargetMatching => enableTargetMatching;
+    public AvatarTarget MatchBodyPart => matchBodyPart;
+    public float MatchStartTime => matchStartTime;
+    public float MatchTargetTime => matchTargetTime;
+    public Vector3 MatchPosWeight => matchPosWeight;
+}
